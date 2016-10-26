1168dbb2c0ce17710e3297331e42b895c267cea7
Ellen <eclinester@gmail.com>
2016-10-26 20:09:33 +0000

    decided not to go fancy, still working on: image, smart quotes, table, etc

--- efcline-convert-docs.sh
+++ efcline-convert-docs.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #Various variables defined:
-ARGUMENT = $1
 
 #Greet the user
-
 read -p "Hello, there! What is your name?" NAME
 echo "Hello," $NAME 
 
@@ -14,27 +12,16 @@ read FILE
 echo "Ok! Let's convert $FILE" 
 
 # Convert markdown to HTML
-echo "Let's convert $FILE to HTML. We can call it html-version-2"
-pandoc -S -o html-version-2.html $FILE
-echo "Here it is!"
+pandoc -S -o $FILE.html $FILE
+# Convert markdown to DOCX, ODT
+pandoc -S -o $FILE.docx $FILE && pandoc -S -o $FILE.odt $FILE
+echo "Here are your new files!"
 ls 
-echo "Continue?"
-read INPUT
-
-# Convert markdown to DOCX
-echo "You answered $INPUT. Now let's convert to word processing files: DOCX and ODT. We will call them microsoft-readable.docx and open-office-readable.odt"
-pandoc -S -o microsoft-readable.docx $FILE
-# Convert markdown to ODT
-pandoc -S -o open-office-readable.odt $FILE
-ls
 
 # Convert markdown to PDF
-echo "We have changed our $FILE to three different formats. Should we try PDF too?"
-read ANSWER
-echo "You answered $ANSWER Let's convert to pdf and get our adobe-version.pdf"
-pandoc -S -toc -o abode-version.pdf html-version.html
-
-echo "Converted $FILE to HTML, DOCX, ODT, PDF. Congratulations! Here are your new files"
+echo "We have changed our $FILE to three different formats. PDF is a little more tricky, but with any luck..."
+pandoc -S -toc -o $FILE.pdf $FILE.html
+echo "Successfully, converted $FILE to HTML, DOCX, ODT, PDF. Congratulations! Here are your new files"
 ls -lah
 
 echo "Returning to home directory in 3, 2, 1..."
