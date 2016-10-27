bf1406d3546526eb6e4bdcb83f5994ce4ec2defb
Ellen <eclinester@gmail.com>
2016-10-27 01:14:52 +0000

    final tweaking in script

--- efcline-convert-docs.sh
+++ efcline-convert-docs.sh
@@ -9,19 +9,24 @@ echo "Hello," $NAME
 echo "This is a script that helps convert files from markdown (.md) to .html, .docx, .odt, and .pdf"
 echo "Which file would you like to convert?" 
 read FILE
-echo "Ok! Let's convert $FILE" 
+echo "Ok, $NAME! Let's convert $FILE" 
 
 # Convert markdown to HTML
-pandoc -S -o $FILE.html $FILE
+pandoc -o $FILE.html $FILE
+echo "Look! We converted a file!"
+ls
+
 # Convert markdown to DOCX, ODT
-pandoc -S -o $FILE.docx $FILE && pandoc -S -o $FILE.odt $FILE
+read -p "DOCX and ODT are two types of files that may be opened in word processors. Let's see what we can do! Press enter to continue."
+pandoc -o $FILE.docx $FILE && pandoc -o $FILE.odt $FILE
 echo "Here are your new files!"
 ls 
 
 # Convert markdown to PDF
-echo "We have changed our $FILE to three different formats. PDF is a little more tricky, but with any luck..."
-pandoc -S -toc -o $FILE.pdf $FILE.html
-echo "Successfully, converted $FILE to HTML, DOCX, ODT, PDF. Congratulations! Here are your new files"
-ls -lah
+echo "We have changed our $FILE to three different formats. PDF is a little more tricky, since it is better to start with an html file."
+read -p "Should we attempt one final file conversion? Press any key to continue!"
+pandoc -o $FILE.pdf $FILE.html
+echo "Successfully converted $FILE to HTML, DOCX, ODT, PDF. Congratulations, $NAME! Here are your new files"
+ls -la
 
 echo "Returning to home directory in 3, 2, 1..."
